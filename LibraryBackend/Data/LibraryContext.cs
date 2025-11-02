3ce9d09565a65a0ab7dbdc0218186d490585a857
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2025-11-02 21:39:33 +0530

    Configure the entity and set maximum values in LibraryContext file

---
+++ LibraryBackend/Data/LibraryContext.cs
@@ -0,0 +1,41 @@
+using LibraryBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryBackend.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+
+        
+        // Represent the Book table in the database
+        public DbSet<Book> Books { get; set; } = null!;
+
+        // Configure the EF Core model and entity relationships
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // configure the Book entity
+            modelBuilder.Entity<Book>(eb =>
+            {
+                // specify the primary key for the Book entity
+                eb.HasKey(b => b.Id);
+
+                // Configure the Title and set maximum length
+                eb.Property(b => b.Title)
+                  .IsRequired()
+                  .HasMaxLength(200);
+
+                // Configure the Author and set maximum length
+                eb.Property(b => b.Author)
+                  .HasMaxLength(200);
+
+                // Configure the ISBN and set maximum length 
+                eb.Property(b => b.ISBN)
+                  .HasMaxLength(50);
+            });
+
+            // Call the base implementation
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
