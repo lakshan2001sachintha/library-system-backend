cba885282331800d9ec5bcbffa81a1424e7ec4fe
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2025-11-04 07:49:05 +0530

    add cors configurations for program.cs file

--- LibraryBackend/Program.cs
+++ LibraryBackend/Program.cs
@@ -2,8 +2,26 @@
 using LibraryBackend.Data;
 using LibraryBackend.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add CORS Configurations
+
+const string AllowReactApp = "AllowReactApp";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowReactApp, policy =>
+    {
+        policy
+           .WithOrigins("http://localhost:5173") // React dev URL
+           .AllowAnyHeader()
+           .AllowAnyMethod()
+           .AllowCredentials();
+    });
+});
+
 
 // Add services to the container
 builder.Services.AddControllers();
@@ -19,6 +37,13 @@ var app = builder.Build();
 
 // Configure middleware
 app.UseHttpsRedirection();
+
+// Apply CORS middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors(AllowReactApp);
+}
+
 app.UseAuthorization();
 app.MapControllers();
 
